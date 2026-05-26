2d720d25543e015e06c2ff8369634224713f4335
Ryukriss-f <adrien.jangal@epfl.ch>
2026-05-26 10:48:26 +0200

    push
    add windows

--- Scopo/Assets/TVGazeController.cs
+++ Scopo/Assets/TVGazeController.cs
@@ -18,19 +18,14 @@ public class TVGazeController : MonoBehaviour
 
     private void Start()
     {
-        // Configuration initiale de base quand on lance le jeu :
-        // Le joueur doit pouvoir se voir à l'écran au début.
         if (normalVideoObject != null) normalVideoObject.SetActive(false);
         if (cameraVideoObject != null) cameraVideoObject.SetActive(true);
     }
 
-    // 1. Appelé par ton script de zone (ZoneCameraTrigger)
     public void SetPlayerInZone(bool inZone)
     {
         isPlayerInZone = inZone;
 
-        // Si le joueur sort complètement de la zone (Zone_Tele), 
-        // on réinitialise l'énigme pour qu'il puisse se re-voir s'il revient.
         if (!isPlayerInZone)
         {
             hasTriggeredSecret = false;
@@ -40,31 +35,24 @@ public class TVGazeController : MonoBehaviour
         }
     }
 
-    // 2. Appelé par la Caméra de surveillance (GazeInteraction d'origine)
     public void StartInteraction()
     {
-        // L'action se déclenche SEULEMENT si le joueur est dans la zone 
-        // ET que le secret n'a pas encore été activé.
         if (isPlayerInZone && !hasTriggeredSecret)
         {
-            hasTriggeredSecret = true; // On verrouille l'action (One-shot tant qu'il reste dans la pièce)
+            hasTriggeredSecret = true;
 
             // LE BASCOULEMENT MAGIQUE :
-            if (cameraVideoObject != null) cameraVideoObject.SetActive(false); // On coupe son propre reflet
-            if (normalVideoObject != null) normalVideoObject.SetActive(true);   // On affiche la vidéo normale
+            if (cameraVideoObject != null) cameraVideoObject.SetActive(false);
+            if (normalVideoObject != null) normalVideoObject.SetActive(true);
 
-            PlayStaticNoise(); // On joue le BZZZT sonore
+            PlayStaticNoise();
         }
     }
 
-    // On n'a plus besoin de rien faire dans StopInteraction ! 
-    // Le changement reste bloqué même si le joueur arrête de regarder la caméra.
     public void StopInteraction() 
     {
-        // On laisse vide volontairement pour maintenir la vidéo normale affichée !
     }
 
-    // 3. Logique Audio standard
     private void PlayStaticNoise()
     {
         if (audioSource != null && tvStaticScratch != null)
